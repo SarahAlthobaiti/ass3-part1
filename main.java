264373cc5d8c47d2d484a05b3a45dee57050149b
SarahAlthobaiti <120081509+SarahAlthobaiti@users.noreply.github.com>
2023-09-13 18:43:16 +0000

    done

---
+++ main.java
@@ -0,0 +1,20 @@
+//Student Name : Sarah Mohammed Althobiti , Student ID : 444004881
+
+
+
+public class main{
+    public static void main(String[] args){
+        Stack<Integer> st = new ArrayStack<>();
+        st.push(5);
+        st.push(3);
+        System.out.println(st.size());
+        System.out.println(st.pop());
+        System.out.println(st.isEmpty());
+        System.out.println(st.pop());
+        System.out.println(st.isEmpty());
+        System.out.println(st.pop());
+
+
+
+    }
+}
